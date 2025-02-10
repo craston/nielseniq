c8898322dcf8cbb608d90cf6d1ee419e2494a9a8
craston <nievescrasto18@gmail.com>
2025-02-10 13:39:42 -0600

    feat: add train command to CLI for main application

--- part_two/cli.py
+++ part_two/cli.py
@@ -1,8 +1,10 @@
 import typer
 import data_handling.splitters as splitters
+import main
 
 app = typer.Typer()
 app.add_typer(splitters.app, name="split", help="Split data")
+app.add_typer(main.app, name="train", help="Main application")
 
 if __name__ == "__main__":
     app()
